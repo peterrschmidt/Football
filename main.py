3297814a81639b50c49a0f093230eb03c627886d
peterrschmidt <peter.robert.schmidt@gmail.com>
2020-05-02 20:38:45 +0100

    add betting data

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 import pickle
-from utils_dataprep import get_team_df, add_features, add_lagged_features, get_feats_labels
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from utils_dataprep import get_team_df, add_features, add_lagged_features, get_match_df,\
+    add_bet_probs
 
 # Load data from pickle
 df = pickle.load(open("C:/Users/peter/data/player_level.pckl", "rb"))
@@ -11,6 +15,7 @@ info_vars = ['home', 'league', 'season', 'date', 'matchid', 'team_name']
 feat_vars = ['goals', 'opp_goals']
 outcome = ['result']
 
+# key of player_df: p_name & matchid
 player_df = df[info_vars + feat_vars + outcome]
 
 # Create team level dataset and add features
@@ -18,12 +23,73 @@ team_df = get_team_df(player_df)
 team_df = add_features(team_df)
 feat_lag_dict = {
     'win_pct': [1],
-    'goals': [1,2],
-    'opp_goals': [1,2]
+    # 'goals': [1,2],
+    # 'opp_goals': [1,2]
 }
+# key of team_df: team_name & matchid
 team_df = add_lagged_features(team_df, feat_lag_dict)
 
-# team_df.loc[(team_df.team_name == "Bayern Munich") & (team_df.season == "15_16")]
+# key of match_df: matchid
+match_df = get_match_df(team_df)
+
+# Add columns with prob of Home Win, Away Win and Draw
+match_df = add_bet_probs(match_df)
+
+match_df.loc[(pd.isna(match_df['B365H'])) & (match_df.season == "15_16"), "league"].unique()
+match_df.loc[np.argmax(match_df['B365H'])]
 
 # Create df with features and series with labels
-features, labels = get_feats_labels(team_df)
+col_mask = [feat for feat in match_df.columns if '_lag_' in feat]
+row_mask = ~match_df[col_mask].isnull().any(axis=1)
+
+features = match_df[row_mask][col_mask]
+labels = match_df[row_mask]['win_home']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    features, labels, test_size=0.33, random_state=42)
+
+lgb_train = lgb.Dataset(X_train, y_train,
+                       free_raw_data=False)
+lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train,
+                       free_raw_data=False)
+
+# specify your configurations as a dict
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'binary',
+    'metric': 'binary_logloss',
+    'num_leaves': 31,
+    'learning_rate': 0.05,
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'verbose': 1
+}
+
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                valid_sets=lgb_train,  # eval training data
+                feature_name=features.columns.to_list()
+                )
+
+print('Feature names:', gbm.feature_name())
+print('Feature importances:', list(gbm.feature_importance()))
+
+y_pred = gbm.predict(X_test)
+print("The rmse of loaded model's prediction is:", mean_squared_error(y_test, y_pred) ** 0.5)
+
+TP = y_test[(y_pred > 0.5) & (y_test == 1)].shape[0]
+FP = y_test[(y_pred > 0.5) & (y_test == 0)].shape[0]
+TN = y_test[(y_pred < 0.5) & (y_test == 0)].shape[0]
+FN = y_test[(y_pred < 0.5) & (y_test == 1)].shape[0]
+
+print("TP: " + str(TP) + " FP: " + str(FP) + "\n"
+      "FN: " + str(FN) + " TN: " + str(TN)
+      )
+
+print("Perc. corr. predicted: " + str(round((TP+TN)/y_test.shape[0], 4)))
+
+team_df.loc[y_test.index[np.argmax(y_pred)]]
+
+team_df.loc[y_test.index[np.argmin(y_pred)]]
--- utils_dataprep.py
+++ utils_dataprep.py
@@ -5,7 +5,6 @@ def get_team_df(
     player_df):
     team_df = player_df.groupby(['matchid', 'team_name']).first().reset_index(drop = False)
     team_df['date'] = pd.to_datetime(team_df['date'])
-    team_df['homeoraway'] = team_df['home'].map({1: 'home', 0: 'away'})
     return team_df
 
 
@@ -33,18 +32,90 @@ def add_lagged_features(
     return team_df
 
 
-def get_feats_labels(
-team_df
-):
+def get_match_df(team_df):
+    team_df['homeoraway'] = team_df['home'].map({1: 'home', 0: 'away'})
     team_df.set_index(['matchid', 'homeoraway'], inplace = True, drop = False)
     match_df = team_df.unstack('homeoraway')
     match_df.columns = ['_'.join(col).strip() for col in match_df.columns.values]
 
-    col_mask = [feat for feat in match_df.columns if '_lag_' in feat]
-    row_mask = ~ match_df[col_mask].isnull().any(axis=1)
+    # These vars are the same for both teams
+    match_vars = ["league", "season", "date"]
+    for var in match_vars:
+        match_df[var] = match_df[var + "_home"]
 
-    features = match_df[row_mask][col_mask]
-    labels = match_df[row_mask]['win_home']
+    # These vars can be dropped
+    for drop_var in match_vars + ["matchid", "home", "homeoraway"]:
+        match_df.drop(drop_var + "_home", axis = 1, inplace = True)
+        match_df.drop(drop_var + "_away", axis = 1, inplace = True)
+        
+    return match_df
 
-    return features, labels
+
+def add_bet_probs(
+    match_df,
+    odds_path = "C:/Users/peter/data/all-euro-data-2015-2016.xls",
+    merge_vars = ['league', 'season', 'team_name_home', 'team_name_away'],
+    odds_vars = ['B365H', 'B365D', 'B365A']
+):
+
+    odds_data = pd.ExcelFile(odds_path)
+    odds_df = pd.DataFrame([])
+    for league in match_df.league.unique():
+        if league in league_dict.keys():
+            temp_df = odds_data.parse(sheet_name = league_dict[league])
+            temp_df["league"] = league
+            temp_df["season"] = "15_16" #TODO: make flexible
+            temp_df["team_name_home"] = temp_df.HomeTeam.map(team_dict).fillna(temp_df['HomeTeam'])
+            temp_df["team_name_away"] = temp_df.AwayTeam.map(team_dict).fillna(temp_df['AwayTeam'])
+        
+        odds_df = pd.concat([odds_df, temp_df], ignore_index = True)
     
+    # Transform odds to probabilities
+    for var in odds_vars:
+        odds_df[var] = 1/odds_df[var]
+
+    match_df = match_df.merge(
+        odds_df[merge_vars + odds_vars],
+        how = 'left',
+        on = merge_vars
+    )
+
+    return match_df
+
+
+
+# Merge with odds data
+league_dict = {
+    "prem_league": "E0",
+    "bundesliga": "D1",
+    "serie_a": "I1",
+    # "laliga": "SP1",
+    # "ligue1": "F1",
+    # "superlig": "T1"
+}
+
+team_dict = {
+'Man City': 'Manchester City', 
+'Man United': 'Manchester United',
+'West Brom': 'West Bromwich Albion',
+'QPR': 'Queens Park Rangers',
+'Wolves': 'Wolverhampton Wanderers',
+'Newcastle': 'Newcastle United',
+'Milan': 'AC Milan',
+'Siena': 'Robur Siena',
+'Dortmund': 'Borussia Dortmund',
+'Hertha': 'Hertha Berlin',
+'Mainz': 'Mainz 05',
+'Nurnberg': 'Nuernberg',
+'Ein Frankfurt': 'Eintracht Frankfurt',
+'FC Koln': 'FC Cologne',
+'Hamburg': 'Hamburger SV', 
+'Hannover': 'Hannover 96',
+'Leverkusen': 'Bayer Leverkusen',
+'Stuttgart': 'VfB Stuttgart',
+"M'gladbach": 'Borussia M.Gladbach',
+'Greuther Furth': 'Greuther Fuerth',
+'Fortuna Dusseldorf': 'Fortuna Duesseldorf',
+'Braunschweig': 'Eintracht Braunschweig',
+'RB Leipzig': 'RasenBallsport Leipzig'
+}
